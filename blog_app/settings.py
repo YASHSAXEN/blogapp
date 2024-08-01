bb62ff745723e22636e23d0b7d01554ec2dcb51d
YASHSAXEN <yashsaxena210403@gmail.com>
2024-08-01 20:29:39 +0530

    update

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jr%4)=+x-h=c^hu36*@6boi)^0oh0034i=2y@^%^vt#o1_u%!^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG',True)
+DEBUG = os.environ.get('DEBUG') == "True"
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','blogging-ly5r.onrender.com']
 
