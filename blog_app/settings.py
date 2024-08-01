261176ca70b46dad65c960c51c01a3dce05a4650
YASHSAXEN <yashsaxena210403@gmail.com>
2024-08-01 20:52:40 +0530

    update settings.py

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','blogging-ly5r.onrender.com']
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
